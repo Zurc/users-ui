ce2e5b1a219c3d12a198ffe420a5b804085d4e64
Cruz Jurado <cruzjurado@gmail.com>
2022-05-22 20:57:13 +0100

    gh-pages

---
+++ docs/users-ui/assets/mock-data.ts
@@ -0,0 +1,30 @@
+const fakeUsers = {
+  data: [
+    {
+      name: "Sam Tin Wong",
+      isEditing: false,
+      id: "0.2b9f2ba48187b",
+    },
+    {
+      name: "Bill Job",
+      isEditing: false,
+      id: "0.7bd659998f6e1",
+    },
+    {
+      name: "Harry James",
+      isEditing: false,
+      id: "0.4bbc2437eafc6",
+    },
+    {
+      name: "Res Berry",
+      isEditing: false,
+      id: "0.1d890a5bc6dfa",
+    },
+    {
+      name: "John Dow",
+      isEditing: false,
+      id: "0.839529ab4059b",
+    },
+  ],
+};
+export default fakeUsers;
